a2105bed010de1bb7cc9861f121f6bcb4763b833
Pradeep Fernando <pradeepfn@gatech.edu>
2018-11-26 14:56:09 -0800

    minor update- script

--- utils-global/setup_hp.sh
+++ utils-global/setup_hp.sh
@@ -18,3 +18,5 @@ mkdir -p /mnt/huge
 mount -t hugetlbfs nodev /mnt/huge
 
 echo 4096 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+${dpdk_home}/tools/dpdk-devbind.py --status
+cat /proc/meminfo | grep Hugepages
