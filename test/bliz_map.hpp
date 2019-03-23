6c748c097ed386ceaa622e6cf3a2360a16f10f42
pradeep <pradeepfn@gmail.com>
2019-03-22 21:20:05 -0700

    blizmap

---
+++ test/bliz_map.hpp
@@ -0,0 +1,28 @@
+#ifndef _BLIZ_MAP_
+#define _BLIZ_MAP_
+
+#include <unistd.h>
+
+#define OP_PUT        0
+#define OP_GET        1
+#define OP_ADD        2
+
+unsigned long blizmap_keys = 100000; // key space used by the benchmark
+
+
+const unsigned long value_sz     = 8;
+typedef struct blizop_st{
+  unsigned long op;
+  unsigned long key;
+  char value[value_sz];
+}blizop_t;
+
+
+static inline int 
+file_exists(char const *file)
+{
+    return access(file, F_OK);
+}
+
+
+#endif
