d8318124c9cc3eaf8a83544197907deaf467f455
Pradeep <pradeepfn@gatech.edu>
2019-09-16 00:41:21 -0400

    implement async calls. Kind of copy pasting. Should improve the code by introducing some common routines.

--- pmemds/src/vector/adjvector-client.cc
+++ pmemds/src/vector/adjvector-client.cc
@@ -10,18 +10,28 @@ namespace pmemdsclient{
         payload.key = from_node;
         payload.key2 = to_node;
         if(!client->sendmsg(&payload,&response,this->core_mask)){
-            LOG_ERROR("hashmap put");
+            LOG_ERROR("adjvector add_edge sync call");
         }
         if(STATUS(response->meta) != OK){
-            LOG_ERROR("hashmap put");
+            LOG_ERROR("adjvector add_edge");
             return FAILED;
         }
         return OK;
     }
 
-	int AdjVectorEngine::add_edge(unsigned long fromnode_id, 
+	int AdjVectorEngine::add_edge(unsigned long fromnode_id,
 			unsigned long tonode_id, void (*cb)(void *)){
-		return 0;//TODO
+        pm_rpc_t *response;
+        pm_rpc_t payload = {0,0,"\0"};
+        SET_DS_ID(payload.meta, this->ds_id);
+        SET_OP_ID(payload.meta,ADD_EDGE);
+        SET_TYPE_ID(payload.meta,this->type_id);
+        payload.key = fromnode_id;
+        payload.key2 = tonode_id;
+        if (client->sendmsg_async(&payload, this->core_mask, cb) != 0) {
+            LOG_ERROR("adjvector add_edge async call");
+        }
+        return OK;
 	}
 
     unsigned long AdjVectorEngine::vertex_outdegree(unsigned long node_id) {
@@ -32,10 +42,10 @@ namespace pmemdsclient{
         SET_TYPE_ID(payload.meta,this->type_id);
         payload.key = node_id;
         if(!client->sendmsg(&payload,&response,this->core_mask)){
-            LOG_ERROR("vertex_outdegree fail");
+            LOG_ERROR("vertex_outdegree sync call");
         }
         if(STATUS(response->meta) != OK){
-            LOG_ERROR("hashmap put");
+            LOG_ERROR("vertex_outdegree");
             return FAILED;
         }
         return response->key;
@@ -43,7 +53,16 @@ namespace pmemdsclient{
 
 
     unsigned long AdjVectorEngine::vertex_outdegree(unsigned long node_id, void (*cb)(void *)){
-		return 0;//TODO:
+        pm_rpc_t *response;
+        pm_rpc_t payload = {0,0,"\0"};
+        SET_DS_ID(payload.meta, this->ds_id);
+        SET_OP_ID(payload.meta,VERTEX_OUTDEGREE);
+        SET_TYPE_ID(payload.meta,this->type_id);
+        payload.key = node_id;
+        if (client->sendmsg_async(&payload, this->core_mask, cb) != 0) {
+            LOG_ERROR("vertex_outdegree async call");
+        }
+        return response->key;
 	}
 
 
@@ -71,6 +90,18 @@ namespace pmemdsclient{
         return OK;
     }
 
+    int AdjVectorEngine::create(uint8_t flags, void (*cb)(void *)) {
+        pm_rpc_t *response;
+        pm_rpc_t payload = {0, 0, "\0"};
+        SET_OP_ID(payload.meta, CREATE_DS);
+        SET_TYPE_ID(payload.meta, this->type_id);
+        SET_DS_ID(payload.meta, this->ds_id);
+        if (client->sendmsg_async(&payload, this->core_mask, cb) != 0) {
+            LOG_ERROR("adjvector create async call failed");
+        }
+        return OK;
+    }
+
     int AdjVectorEngine::close() {
         pm_rpc_t *response;
         pm_rpc_t payload = {0, 0, "\0"};
@@ -87,15 +118,19 @@ namespace pmemdsclient{
         return OK;
     }
 
-    int AdjVectorEngine::remove() {
-        return 0;
-    }
-
     int AdjVectorEngine::close(void (*cb)(void *)) {
-        return 0;
+        pm_rpc_t *response;
+        pm_rpc_t payload = {0, 0, "\0"};
+        SET_OP_ID(payload.meta, CLOSE_DS);
+        SET_TYPE_ID(payload.meta, this->type_id);
+        SET_DS_ID(payload.meta, this->ds_id);
+        if (client->sendmsg_async(&payload, this->core_mask, cb) != 0) {
+            LOG_ERROR("adjvector create async call failed");
+        }
+        return OK;
     }
 
-    int AdjVectorEngine::create(uint8_t flags, void (*cb)(void *)) {
+    int AdjVectorEngine::remove() {
         return 0;
     }
 
