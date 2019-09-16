bac49302d74cd79cea17c55924a34bf57c78226c
Pradeep <pradeepfn@gatech.edu>
2019-09-16 13:30:28 -0400

    commute logic for twitter

--- benchmarks/twitter/adjvector/adjvector_server.cpp
+++ benchmarks/twitter/adjvector/adjvector_server.cpp
@@ -35,6 +35,17 @@ int commute_callback(void *arg1, void *arg2) {
     /* this is the signle partition data-structure. So we ignore partitions */
 	unsigned int op1_id = OP_ID(op1->meta);
 	unsigned int op2_id = OP_ID(op2->meta);
+	/// read operations always commute
+	if(op1_id == VETEX_OUTDEGREE && op2_id == VERTEX_OUTDEGREE){
+		return 1;
+	}
+	/* if the operations are in to different nodes, then they commute.
+	 * 1. provide strong consistent reads vetex_outdegree
+	 * 2. relaxed reads for incident triangles and hop
+	 */ 
+	if( (op1->key != op2->key){
+		return 1;
+	}	
 	return 0;
 }
 
