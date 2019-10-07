1585339dda6b8f3579b0ce71fed63ce1c01362fa
pradeep <pradeepfn@gmail.com>
2019-10-07 16:50:20 -0700

    kvbench memcached final configs

--- benchmarks/kvbench/memcached/memcached_kvbench.sh
+++ benchmarks/kvbench/memcached/memcached_kvbench.sh
@@ -5,12 +5,14 @@ memaslap=/home/pfernando/libmemcached-1.0.18/clients/memaslap
 result_dir=../../../utils-global/results/kvbench/memcached
 
 #memcached params
-memory=2024
+memory=10240
 port=11211
-host=127.0.0.1
+threads=7
+host=localhost
 
-declare -a thr=(1 2 4 8)
-
+declare -a thr=(1 2 4 6 8 8 8 8 8)
+declare -a conc=(1 2 4 6 8 16 32 48 64)
+index=0
 
 #memaslap params
 running_time=1m
@@ -20,10 +22,10 @@ for nth in  "${thr[@]}"
 do
 mkdir -p $result_dir/t"$nth"
 
-
-numactl --cpubind=1 --membind=1 ${memcached} -m $memory  nobody -p $port -l $host 1 & > memcachedserver.log &
-${memaslap} -s $host:$port -S 5s --time=$running_time --threads=$nth --cfg_cmd=$cfg_file > $result_dir/t"$nth"/memslap.log
+echo "threads:" $nth "concurrency:" ${conc[$index]}
+numactl --cpubind=1 --membind=1 ${memcached} -m $memory -t $threads  nobody -p $port -l $host 1 & > memcachedserver.log &
+numactl --cpubind=0 --membind=0 ${memaslap} -s $host:$port -S 5s --time=$running_time --threads=$nth --concurrency=${conc[$index]} --binary --cfg_cmd=$cfg_file > $result_dir/t"$nth"/memslap"${conc[$index]}".log
 pkill -f memcached
 sleep 5;
-
+((index++))
 done
