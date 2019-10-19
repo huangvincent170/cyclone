c1fe7260b771bde3650f1934e10a726bc4860aa2
pradeep <pradeepfn@gmail.com>
2019-10-18 18:19:25 -0700

    pmemkv and memcached bench for crash-sync plot

---
+++ scripts/crash-sync/memcached.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# install memcached
+# install libmemcached for memasalap client
+
+
+
+memcached=/usr/local/memcached/bin/memcached
+#memaslap=/home/pradeep/installations/libmemcached-1.0.18/clients/memaslap
+memaslap=/home/pfernando/libmemcached-1.0.18/clients/memaslap
+result_dir=../../../utils-global/results/kvbench/memcached
+
+#memcached params
+memory=10240
+port=11211
+threads=7
+host=localhost
+
+declare -a thr=(1 2 4 6 8 8 8 8 8)
+declare -a conc=(1 2 4 6 8 16 32 48 64)
+index=0
+
+#memaslap params
+running_time=1m
+cfg_file=memaslap.conf
+
+for nth in  "${thr[@]}"
+do
+mkdir -p $result_dir/t"$nth"
+
+echo "threads:" $nth "concurrency:" ${conc[$index]}
+numactl --cpubind=1 --membind=1 ${memcached} -m $memory -t $threads  nobody -p $port -l $host 1 & > memcachedserver.log &
+numactl --cpubind=0 --membind=0 ${memaslap} -s $host:$port -S 5s --time=$running_time --threads=$nth --concurrency=${conc[$index]} --binary --cfg_cmd=$cfg_file > $result_dir/t"$nth"/memslap"${conc[$index]}".log
+pkill -f memcached
+sleep 5;
+((index++))
+done
---
+++ scripts/crash-sync/pmemkv.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# download pmemkv bench from https://github.com/pmem/pmemkv-tools.git
+# download and install pmemkv from https://github.com/pmem/pmemkv.git
+# download and install following software versions
+# pmdk 1.7
+# libpmeobj-cpp 1.8
+# pmemkv 1.0
+# pmemkv-tools (master)
+
+declare -a rwmix=(0 50)
+declare -a nthreads=(1 2 4 8)
+
+db_file=/mnt/pmem1/pmemkv
+#db_file=./dbpmem
+#db_file=/mnt/pmem1/pmemkv
+
+COMMON_EXEC="PMEM_IS_PMEM_FORCE=1 ./pmemkv-tools/pmemkv_bench
+--db=/mnt/pmem1/pmemkv
+--db_size_in_gb=2
+--key_size=8
+--value_size=8"
+
+mkdir -p results/filebench
+for rw in "${rwmix[@]}"
+do
+	rm /mnt/pmem1/pmemkv
+	
+	eval $COMMON_EXEC "--benchmarks=fillseq,readrandomwriterandom --readwritepercent=$rw"
+
+	mkdir -p results/randomrw
+	for th in "${nthreads[@]}"
+	do
+		eval $COMMON_EXEC "--benchmarks=readrandomwriterandom --readwritepercent=$rw --threads=$th" > results/randomrw/pmemkv_${rw}_${th}.out 
+	done
+done
