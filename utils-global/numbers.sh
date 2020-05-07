4efccdedb4ec41709bd8a6b5722cd8c1f7a61c5e
pradeep <pradeepfn@gmail.com>
2020-05-07 10:36:59 -0700

    configs

--- utils-global/numbers.sh
+++ utils-global/numbers.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 rm -rf test
-python config_generator.py ../utils-arch-cluster/cluster-dpdk.ini.1 ../utils-arch-cluster/example.ini.1 ./echo.py test
+#python config_generator.py ../utils-arch-cluster/aep-machines.ini.1 ../utils-arch-cluster/aep-example.ini.1 ./echo.py test
+#python config_generator.py ../utils-arch-cluster/cluster-dpdk.ini.1 ../utils-arch-cluster/example.ini.1 ./echo.py test
+python config_generator.py ../utils-arch-cluster/aep-machines.ini.2 ../utils-arch-cluster/aep-example.ini.2 ./echo.py test
 
 ./deploy_configs.sh test /home/pfernando/cyclone
 ./copy_binaries.sh test /home/pfernando/cyclone echo
