23e9ab635ccaca691631f7ee7f40b436696c260b
pradeep <pradeepfn@gatech.edu>
2020-05-13 00:08:42 -0700

    scripts

---
+++ scripts/vote/prioqueue.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#declare -a wl=("hashmap")
+declare -a wl=("pqueue")
+declare -a bf=(1 2 4 6 8 10 12 14)
+
+declare -a mt=("nvram")
+
+#declare -r rl=(1)
+declare -r rl=(3)
+
+for replicas in "${rl[@]}"
+do
+for w in "${wl[@]}"
+do
+  for m in "${mt[@]}"
+  do
+	for b in "${bf[@]}"
+	do
+    ./vote_bench.py -c 
+    ./vote_bench.py -g -rep "$replicas" -m "$m" -w "$w" -b "$b"
+    ./vote_bench.py -dc -m "$m" -w "$w"
+    ./vote_bench.py -db -m "$m" -w "$w"
+
+    ./vote_bench.py -startsrv -m "$m" -w "$w"
+    ./vote_bench.py -startclnt -m "$m" -w "$w"
+	# wait for sometime
+	sleep 60
+    ./vote_bench.py -stop -m "$m" -w "$w"
+
+	# gather output
+    ./vote_bench.py -collect -rep "$replicas" -m "$m" -w "$w" -b "$b"
+	done
+  done
+done
+done
+exit
---
+++ scripts/vote/vote_bench.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+import argparse
+from commonbench import *
+
+#workloads
+pqueue = 'pqueue'
+
+wl=[]
+wl.append(pqueue)
+
+wl.append(empty)
+
+
+parser = argparse.ArgumentParser(prog="runscript", description="script to run cyclone for numbers")
+parser.add_argument('-c', dest='clean', action='store_true', default=False, help="clean env after a run")
+parser.add_argument('-g', dest='gen', action='store_true', default=False, help="generate config")
+parser.add_argument('-collect', dest='collect', action='store_true', default=False, help="collect output")
+parser.add_argument('-db', dest='deploy_bins', action='store_true', default=False, help="deploy client/server binaries")
+parser.add_argument('-dc', dest='deploy_configs', action='store_true', default=False, help="deploy configs")
+parser.add_argument('-startsrv', dest='startsrv', action='store_true', default=False, help="run experiment")
+parser.add_argument('-startclnt', dest='startclnt', action='store_true', default=False, help="run experiment")
+parser.add_argument('-stop', dest='stop', action='store_true', default=False, help="stop experiment")
+parser.add_argument('-w', dest='workload', default=empty , help='workload name, eg: echo, pmemkv', choices=wl)
+parser.add_argument('-m', dest='memtype', default=empty , help='memory type', choices=ml)
+parser.add_argument('-b', dest='bufsize', default=empty , help='inflight buffer size')
+parser.add_argument('-rep', dest='replicas', default=empty , help='number of replicas', choices=rl)
+parser.add_argument('-commute', dest='is_commute', action='store_true', default=False , help='concurrent execution')
+
+try:
+    args = parser.parse_args()
+
+except:
+    dbg('Error parsing input')
+    sys.exit(0)
+
+class Vote(Common):
+    #map some workload names to binary name
+    def wl2binary(self,arg):
+        switcher= {
+            'pqueue_ncc' : 'pqueue',
+        }
+        return switcher.get(arg,arg)
+
+    def bench(self):
+        return 'vote';
+
+    def get_bench_dir(self):
+        return 'ERROR'
+
+    def get_server_cxx(self,wload):
+        return '' #else
+
+if __name__ == '__main__':
+
+    c = args.clean
+    g = args.gen
+    strt_serv = args.startsrv
+    strt_clnt = args.startclnt
+    stop = args.stop
+    db = args.deploy_bins
+    dc = args.deploy_configs
+    clct = args.collect
+
+    vt = Vote();
+
+
+    if args.clean is True:
+        vt.clean(args)
+    if db == True:
+        vt.deploy_bin(args)
+    if g == True:
+        vt.generate(args)
+    if dc == True:
+        vt.deploy_configs(args)
+    if strt_serv == True:
+        vt.start_cyclone_server(args)
+    if strt_clnt == True:
+        vt.start_cyclone_client(args)
+    if stop == True:
+        vt.stop_cyclone(args)
+    if clct == True:
+        vt.gather_output(args)
